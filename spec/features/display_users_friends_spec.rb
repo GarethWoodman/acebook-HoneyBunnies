ae40f3589eba815d2ccc1a7835829b16ba078b52
Catriona <catrionamairi@blueyonder.co.uk>
2020-06-11 20:07:30 +0100

    Rubocop issues fixed

--- spec/features/display_users_friends_spec.rb
+++ spec/features/display_users_friends_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.feature 'Display Users friends', type: :feature do
   scenario 'See list of friends on user profle' do
-    jon = User.create(fname: 'Jon', lname: 'Doe', email: 'john.doe@example.com', password: 'password')
+    User.create(fname: 'Jon', lname: 'Doe', email: 'john.doe@example.com', password: 'password')
     visit '/sessions/new'
 
     fill_in 'user[email]', with: 'john.doe@example.com'
