96f921c78c2b1778215e7ed5f22dfb5e4a8be759
Ed <lookatemail@gmail.com>
2020-06-11 15:25:22 +0100

    able to delete image from post

--- spec/features/user_able_to_update_post_spec.rb
+++ spec/features/user_able_to_update_post_spec.rb
@@ -23,31 +23,26 @@ RSpec.feature 'Update Post', type: :feature do
     expect(page).to have_content('New Message!')
   end
 
-  # scenario 'user is able to delete a picture' do
-  #   user = User.create(fname: 'Jon', lname: 'Doe', email: 'john.doe@example.com', password: 'password')
+  scenario 'user is able to delete a picture' do
+    user = User.create(fname: 'Jon', lname: 'Doe', email: 'john.doe@example.com', password: 'password')
 
-  #   visit '/sessions/new'
+    visit '/sessions/new'
 
-  #   fill_in 'user[email]', with: 'john.doe@example.com'
-  #   fill_in 'user[password]', with: 'password'
-  #   click_button 'Sign in'
+    fill_in 'user[email]', with: 'john.doe@example.com'
+    fill_in 'user[password]', with: 'password'
+    click_button 'Sign in'
 
-  #   # p '**********'
-  #   # p post.images.record.id
-  #   # p '***********'
+    fill_in 'Message', with: 'Hello, world!'
+    attach_file 'post[images][]', "#{Rails.root}/spec/files/MVC_DataB.png"
+    click_button 'Submit'
 
-  #   fill_in 'Message', with: 'Hello, world!'
-  #   attach_file 'post[images][]', "#{Rails.root}/spec/files/MVC_DataB.png"
-  #   click_button 'Submit'
+    all(:link, 'Update Post').last.click
 
-  #   all(:link, 'Update Post').last.click
+    click_button 'Delete'
+    click_button 'Submit'
 
-  #   click_button 'Delete'
-  #   # attach_file 'post[images][]', "#{Rails.root}/spec/files/Matterhorn.jpg"
-  #   click_button 'Submit'
-
-  #   expect(current_path).to eq '/posts'
-  #   expect(page).to have_content('Hello, world!')
-  #   expect(page).to_not have_css(".rounded")
-  # end
+    expect(current_path).to eq '/posts'
+    expect(page).to have_content('Hello, world!')
+    expect(page).to_not have_css(".rounded")
+  end
 end
