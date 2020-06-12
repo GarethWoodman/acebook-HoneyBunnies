d252af957cb3a810eb60fdc9efb107d379326e78
Catriona <catrionamairi@blueyonder.co.uk>
2020-06-12 14:49:01 +0100

    Minor changes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,13 +4,13 @@ class LikesController < ApplicationController
 
   def create
     @like = Like.new(user_id: session[:user_id], post_id: params[:post_id])
-    #redirect_to posts_url if @like.save
+    # redirect_to posts_url if @like.save
     redirect_to params[:path] if @like.save
   end
 
   def destroy
     @like = Like.find_by(user_id: params[:user_id], post_id: params[:post_id])
-    #redirect_to posts_url
+    # redirect_to posts_url
     redirect_to params[:path] if @like.destroy
   end
 end
